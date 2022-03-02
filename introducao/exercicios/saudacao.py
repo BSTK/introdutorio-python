ddf42564e73347c9e185df05930937230b7d4c34
Bruno Luz <bsilva.se@gmail.com>
2022-03-02 20:17:26 -0300

    main - Primeiros exemplos

---
+++ introducao/exercicios/saudacao.py
@@ -0,0 +1,13 @@
+# Exercicio 2 - Saudações
+
+print('\n')
+print('\t▄ █ ▄ █ ▄ ▄ █ ▄ █ ▄ █ ▄ █ ▄ █ ▄')
+print('\t▀   * Iniciando com Python *  ▀')
+print('\t▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀')
+
+nome = input('\tQual seu nome: ')
+idade = input('\tQual sua idade: ')
+
+print('\n')
+print(f'\t♫ ♪ Bem vindo { nome }, parabéns pelo seus { idade } anos! ♫ ♪')
+print('\n')
---
+++ introducao/trabalhando_com_print_e_input.py
@@ -0,0 +1,12 @@
+# Função para printar texto na tela
+print('Iniciando com Python!')
+print('Primeiros exemplos!')
+
+print('\n')
+
+# Função para entrada de dados
+nome = input('Qual seu nome? ')
+idade = input('Qual sua idade? ')
+
+print('Nome: ', nome)
+print('Idade: ', idade)
